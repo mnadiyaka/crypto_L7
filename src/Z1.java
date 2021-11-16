7f6dc421f73f19110b8dc9ede96b9f14ef436780
Nadiia <mnadiyaka@gmail.com>
2021-11-16 20:39:24 +0200

    task 1 done

--- src/Z1.java
+++ src/Z1.java
@@ -1,8 +1,9 @@
-import java.util.concurrent.ThreadLocalRandom;
+import java.math.BigInteger;
+import java.security.SecureRandom;
 
 public class Z1 {
     public static void main(String[] args) {
-        System.out.println(testMR(3547)+"%");
+        System.out.println(testMR(3463)+"%");
     }
 
     public static int testMR(int n) {
@@ -27,25 +28,36 @@ public class Z1 {
         System.out.println("s="+s+" t="+t);
         m: for (int i = 0; i < r; i++)
         {
-            int a = ThreadLocalRandom.current().nextInt(2, (n-2) + 1);;
+            BigInteger a;
+            SecureRandom random = new SecureRandom();
+            byte bytes[];// = new byte[Integer.toBinaryString(n).length()];
+            //random.nextBytes(bytes);
+            do
+            {
+                bytes = random.generateSeed(s+1);
+                a = new BigInteger(bytes);
+                //System.out.println(a);
+            }
+            while (a.compareTo(BigInteger.valueOf(2)) <0 || a.compareTo(BigInteger.valueOf(n - 2))>0);
             System.out.println("a=" +a);
-            int x = ((int) Math.pow(a, t))%n;
-            if (x == 1 || x == n - 1) {
+            BigInteger x = a.modPow(BigInteger.valueOf(t),BigInteger.valueOf(n));
+            if (x.compareTo(BigInteger.valueOf(1)) == 0 || x.compareTo(BigInteger.valueOf(n - 1))==0) {
                 persentage++;
                 continue;
             }
             for (int j = 0; j < s - 1; j++)
             {
-                x = (x*x)%n;
-                if (x == 1)
+                x = x.modPow(BigInteger.valueOf(2),BigInteger.valueOf(n));
+                if (x.compareTo(BigInteger.valueOf(1)) == 0)
                     return 0;
-                if (x == n - 1) {
+                if (x.compareTo(BigInteger.valueOf(n - 1))==0) {
                     persentage++;
                     break ;
                 }
             }
-            if (x != n-1)
-                return 0;
+            if (x.compareTo(BigInteger.valueOf(n - 1))!=0) {
+                return  0;
+            }
         }
         return (persentage-1)/r*100;
     }
